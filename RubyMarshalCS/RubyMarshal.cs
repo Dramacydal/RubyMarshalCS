97f4d5503337dc999a27fb3a136b63a141f6de7b
Dramacydal <PulLumBerMal@gmail.com>
2024-04-29 05:05:52 +0300

    add more marshal methods

--- RubyMarshalCS/RubyMarshal.cs
+++ RubyMarshalCS/RubyMarshal.cs
@@ -6,12 +6,20 @@ namespace RubyMarshalCS;
 public static class RubyMarshal
 {
     private const ushort HeaderMagic = 0x804;
-    
+
+    public static T? Load<T>(byte[] bytes, SerializationSettings? settings = null)
+    {
+        return RubyDeserializer.Deserialize<T>(Load(bytes, settings), settings);
+    }
+
     public static T? Load<T>(string path, SerializationSettings? settings = null)
     {
-        using var reader = new BinaryReader(File.OpenRead(path));
+        return RubyDeserializer.Deserialize<T>(Load(path, settings), settings);
+    }
 
-        return Load<T>(reader, settings);
+    public static T? Load<T>(Stream stream, SerializationSettings? settings = null)
+    {
+        return RubyDeserializer.Deserialize<T>(Load(stream, settings), settings);
     }
 
     public static T? Load<T>(BinaryReader reader, SerializationSettings? settings = null)
@@ -19,13 +27,25 @@ public static class RubyMarshal
         return RubyDeserializer.Deserialize<T>(Load(reader, settings), settings);
     }
 
+    public static AbstractEntity Load(byte[] bytes, SerializationSettings? settings = null)
+    {
+        using var stream = new MemoryStream(bytes);
+
+        return Load(stream, settings);
+    }
+
     public static AbstractEntity Load(string path, SerializationSettings? settings = null)
     {
-        using var reader = new BinaryReader(File.OpenRead(path));
+        return Load(File.OpenRead(path), settings);
+    }
+
+    public static AbstractEntity Load(Stream stream, SerializationSettings? settings = null)
+    {
+        using var reader = new BinaryReader(stream);
 
         return Load(reader, settings);
     }
-
+    
     public static AbstractEntity Load(BinaryReader reader, SerializationSettings? settings = null)
     {
         var version = reader.ReadUInt16();
@@ -35,23 +55,42 @@ public static class RubyMarshal
         return new SerializationContext(settings).Read(reader);
     }
 
-    public static void Dump<T>(string path, T? obj)
+    public static void Dump<T>(string path, T? obj, SerializationSettings? settings = null)
     {
-        var entity = RubySerializer.Serialize(obj);
+        var entity = RubySerializer.Serialize(obj, settings);
         
         Dump(path, entity);
     }
 
-    public static void Dump<T>(BinaryWriter writer, T? obj)
+    public static void Dump<T>(Stream stream, T? obj, SerializationSettings? settings = null)
     {
-        var entity = RubySerializer.Serialize(obj);
+        var entity = RubySerializer.Serialize(obj, settings);
+        
+        Dump(stream, entity);
+    }
+    
+    public static void Dump<T>(BinaryWriter writer, T? obj, SerializationSettings? settings = null)
+    {
+        var entity = RubySerializer.Serialize(obj, settings);
         
         Dump(writer, entity);
     }
-    
+
+    public static byte[] Dump<T>(T? obj, SerializationSettings? settings = null)
+    {
+        var entity = RubySerializer.Serialize(obj, settings);
+        
+        return Dump(entity);
+    }
+
     public static void Dump(string path, AbstractEntity entity)
     {
-        using var writer = new BinaryWriter(File.Open(path, FileMode.Create));
+        Dump(File.Open(path, FileMode.Create), entity);
+    }
+
+    public static void Dump(Stream stream, AbstractEntity entity)
+    {
+        using var writer = new BinaryWriter(stream);
 
         Dump(writer, entity);
     }
@@ -62,5 +101,14 @@ public static class RubyMarshal
 
         entity.Context.Reset();
         entity.Context.Write(writer, entity);
+    }
+
+    public static byte[] Dump(AbstractEntity entity)
+    {
+        using var stream = new MemoryStream();
+
+        Dump(stream, entity);
+
+        return stream.GetTrimmedBuffer();
     }
 }
